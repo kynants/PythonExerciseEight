17295aef97761a68f9e8cce166098501d587869a
Kynan Stevenson <kynants@gmail.com>
2020-10-09 05:10:22 -0400

    Initial commit

---
+++ program.py
@@ -0,0 +1,6 @@
+color_list = ["Red", "Green", "White", "Black"]
+
+# https://www.geeksforgeeks.org/python-get-first-and-last-elements-of-a-list/
+first_and_last_items = [color_list[0], color_list[-1]]
+
+print("color_list: ", first_and_last_items)
